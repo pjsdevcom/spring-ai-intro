2b26614df0fc7589d6ade35caa4f9a66426f626e
pjsdevcom <mail@pjsdev.com>
2025-03-22 15:58:32 +0000

    first commit

---
+++ src/main/java/com/pjsdev/springaiintro/SpringAiIntroApplication.java
@@ -0,0 +1,13 @@
+package com.pjsdev.springaiintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiIntroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pjsdev/springaiintro/SpringAiIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjsdev.springaiintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
